086d9d48d3cac2614988a920a76c02a33fb3914a
Jennie Duffer <jenniej297@gmail.com>
2022-08-09 16:49:26 -0500

    initial commit

---
+++ OperationExercise/Exercise 2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OperationExercise
+{
+    internal class Exercise2
+    {
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+
+
+
+        }
+    }
+
+
+}
---
+++ OperationExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//int a = 17;
+//int b = 4;
+
+
+//int quotient = a / b;
+//int remainder = a % b;
+//if (a == 17 && b == 4)
+
+// Console.WriteLine($"{a} /= {b} is {quotient} remainder of {remainder}");
+
+using OperationExercise;
+
+Console.WriteLine("What is the radius of your circle?");
+var userInput = Console.ReadLine();
+var radius = double.Parse(userInput);
+Console.WriteLine(Exercise2.AreaOfCircle(radius));
+
+
+
+
+
+
+
